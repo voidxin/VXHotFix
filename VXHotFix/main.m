b936e9c74c2e711ee5a35291dc6375de9fb102a5
voidxin <zhangxin409999@163.com>
2016-12-07 11:12:14 +0800

    Initial Commit

---
+++ VXHotFix/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VXHotFix
+//
+//  Created by voidxin on 16/12/7.
+//  Copyright © 2016年 wugumofang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
